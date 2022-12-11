e5e7668c74da03f2efd9ff72f52f2fc60dcbf824
simon-k1993 <120113010+simon-k1993@users.noreply.github.com>
2022-12-11 22:10:04 +0100

    Add files via upload

---
+++ domasna 3-JS/script.js
@@ -0,0 +1,9 @@
+function input_parameter(value) {
+  console.log("The added value is: ", typeof value);
+}
+
+input_parameter(null);
+input_parameter(12);
+input_parameter(5 > 2);
+input_parameter("number");
+input_parameter();
