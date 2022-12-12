d5bb8ba7d94d33f0df3ba092cee000a9c2bf7008
simon-k1993 <120113010+simon-k1993@users.noreply.github.com>
2022-12-12 01:08:36 +0100

    Delete domasna 3-JS directory

--- domasna 3-JS/script.js
+++
@@ -1,9 +0,0 @@
-function input_parameter(value) {
-  console.log("The added value is: ", typeof value);
-}
-
-input_parameter(null);
-input_parameter(12);
-input_parameter(5 > 2);
-input_parameter("number");
-input_parameter();
