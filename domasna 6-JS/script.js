177c031cd72365a1602f5a1f6c11b6c2df7cbcac
simon-k1993 <120113010+simon-k1993@users.noreply.github.com>
2022-12-14 13:05:47 +0100

    Add files via upload

---
+++ domasna 6-JS/script.js
@@ -0,0 +1,16 @@
+function tellStory(story) {
+  console.log(
+    "This is " +
+      story[0] +
+      ", " +
+      story[0] +
+      " is " +
+      story[1] +
+      " because he is " +
+      story[2] +
+      "!!!"
+  );
+}
+
+let array = ["Simon", "happy", "coding"];
+tellStory(array);
