1a2ee5d2e531986e5ad8797b7530eb762c4eaaf9
simon-k1993 <120113010+simon-k1993@users.noreply.github.com>
2022-12-14 13:06:19 +0100

    Add files via upload

---
+++ domasna 8-JS/script.js
@@ -0,0 +1,16 @@
+function arrayOfSrings(string) {
+  console.log(array.join(" "));
+}
+
+let array = [
+  "This",
+  "is",
+  "Simon",
+  "and",
+  "this",
+  "is",
+  "his",
+  "homework",
+  "!!!",
+];
+arrayOfSrings(array);
